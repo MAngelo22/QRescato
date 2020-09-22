4aaf278e4cb940e33a5a13e36f5f2bd80d616785
Miguel Angel <Miguelangelnunezlopez96@gmail.com>
2020-09-22 19:09:37 +0200

    No carga camara

--- app/src/main/java/com/example/qrescato/CamaraQR.java
+++ app/src/main/java/com/example/qrescato/CamaraQR.java
@@ -19,7 +19,7 @@ public class CamaraQR extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.camaraqr);
 
         btnScanner = findViewById(R.id.button);
         tvBArCode = findViewById(R.id.texto);
--- app/src/main/java/com/example/qrescato/MenuRastreo.java
+++ app/src/main/java/com/example/qrescato/MenuRastreo.java
@@ -1,13 +1,29 @@
 package com.example.qrescato;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuRastreo extends AppCompatActivity {
+
+    Button BtnVis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rastreo);
+
+        BtnVis = findViewById(R.id.buttonvis);
     }
+
+    public void Visita(View view) {
+
+        Intent cambioSp1 = new Intent(this, CamaraQR.class);
+        startActivity(cambioSp1);
+    }
+
 }
+
