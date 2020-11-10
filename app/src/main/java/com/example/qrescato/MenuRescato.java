41ddd113cbc756aea21bb43c120b7a8985708de3
Miguel Angel <Miguelangelnunezlopez96@gmail.com>
2020-11-10 21:13:43 +0100

    CAmbio mapa y skin de app

--- app/src/main/java/com/example/qrescato/MenuRescato.java
+++ app/src/main/java/com/example/qrescato/MenuRescato.java
@@ -1,16 +1,33 @@
 package com.example.qrescato;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressLint("Registered")
 public class MenuRescato extends AppCompatActivity {
 
+    Button Visi;
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rescato);
+
+        Visi = findViewById(R.layout.activity_maps);
+
     }
+
+    public void mapa (View view) {
+
+        Intent cambioSp1 = new Intent(MenuRescato.this, MapsActivity.class);
+        startActivity(cambioSp1);
+    }
+
 }
 
